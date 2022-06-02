39fb562571e0e27e6d45671bd50e5546f26fb3c7
douglasmoura <douglasmoura2@outlook.com>
2022-06-02 14:01:44 -0300

    Correções formatação

--- main.js
+++ main.js
@@ -3,21 +3,19 @@ let atributos = document.getElementById('atributos');
 let resultado = document.querySelector('div#result');
 
 function atributosForca(){
-    
     let pontosAtributos = atributos.value;
     let danoCritico = 0.2;
     let ataqueFisico = 2.6;
     let ataqueDuplo = 0.15;
     let perfuracaoFisica = 0.3;
 
-    let CalculoDanoCritico = Number(danoCritico) * Number(pontosAtributos);
-    let CalculoAtaqueFisico = Number(ataqueFisico) * Number(pontosAtributos);
-    let CalculoAtaqueDuplo = Number(ataqueDuplo) * Number(pontosAtributos);
-    let CalculoPerfuracaoFisica =  Number(perfuracaoFisica) * Number(pontosAtributos);
+    let CalculoDanoCritico = (Number(danoCritico) * Number(pontosAtributos)).toFixed(2);
+    let CalculoAtaqueFisico = (Number(ataqueFisico) * Number(pontosAtributos)).toFixed(2);
+    let CalculoAtaqueDuplo = (Number(ataqueDuplo) * Number(pontosAtributos)).toFixed(2);
+    let CalculoPerfuracaoFisica =  (Number(perfuracaoFisica) * Number(pontosAtributos)).toFixed(2);
     
-  
     resultado.innerHTML = `${CalculoDanoCritico} Dano crítico<br> 
-                           ${CalculoAtaqueFisico} Ataque físico<br>
+                           ${CalculoAtaqueFisico} Ataque físico (curto alcance)<br>
                            ${CalculoAtaqueDuplo} Ataque duplo<br>
                            ${CalculoPerfuracaoFisica}% Perfuração física`;
 }
@@ -29,14 +27,13 @@ function atributosAgilidade(){
     let taxaAcerto = 0.15;
     let esquiva= 0.05;
 
-    let CalculoAtaqueCritico = Number(ataqueCritico) * Number(pontosAtributos);
-    let CalculoAtaqueFisico = Number(ataqueFisico) * Number(pontosAtributos);
-    let CalculoTaxaAcerto = Number(taxaAcerto) * Number(pontosAtributos);
-    let CalculoEsquiva =  Number(esquiva) * Number(pontosAtributos);
-    
+    let CalculoAtaqueCritico = (Number(ataqueCritico) * Number(pontosAtributos)).toFixed(2);
+    let CalculoAtaqueFisico = (Number(ataqueFisico) * Number(pontosAtributos)).toFixed(2);
+    let CalculoTaxaAcerto = (Number(taxaAcerto) * Number(pontosAtributos)).toFixed(2);
+    let CalculoEsquiva =  (Number(esquiva) * Number(pontosAtributos)).toFixed(2);  
   
     resultado.innerHTML = `${CalculoAtaqueCritico} Ataque crítico<br> 
-                           ${CalculoAtaqueFisico} Ataque físico<br>
+                           ${CalculoAtaqueFisico} Ataque físico (longo alcance)<br>
                            ${CalculoTaxaAcerto} Taxa de acerto<br>
                            ${CalculoEsquiva} Esquiva`;
  
@@ -49,12 +46,11 @@ function atributosSorte(){
     let resistenciaStatusAnormais = 0.1;
     let habilidadeAtaque = 5;
 
-    let CalculoMpMaximo = Number(mpMaximo) * Number(pontosAtributos);
-    let CalculoRegeneracaoMp = Number(regeneracaoMp) * Number(pontosAtributos);
-    let CalculoResistenciaStatusAnormais = Number(resistenciaStatusAnormais) * Number(pontosAtributos);
-    let CalculoHabilidadeAtaque =  Number(habilidadeAtaque) * Number(pontosAtributos);
+    let CalculoMpMaximo = (Number(mpMaximo) * Number(pontosAtributos)).toFixed(2);
+    let CalculoRegeneracaoMp = (Number(regeneracaoMp) * Number(pontosAtributos)).toFixed(2);
+    let CalculoResistenciaStatusAnormais = (Number(resistenciaStatusAnormais) * Number(pontosAtributos)).toFixed(2);
+    let CalculoHabilidadeAtaque =  (Number(habilidadeAtaque) * Number(pontosAtributos)).toFixed(2);
     
-  
     resultado.innerHTML = `${CalculoMpMaximo} MP máximo<br> 
                            ${CalculoRegeneracaoMp} Regeneração de MP<br>
                            ${CalculoResistenciaStatusAnormais} Resistência de status anormais<br>
@@ -69,12 +65,11 @@ function atributosInteligencia(){
     let resfriamento = 0.04;
     let perfuracaoMagica = 0.3;
 
-    let CalculoAtaqueMagico = Number(ataqueMagico) * Number(pontosAtributos);
-    let CalculoCura = Number(cura) * Number(pontosAtributos);
-    let CalculoResfriamento = Number(resfriamento) * Number(pontosAtributos);
-    let CalculoPerfuracaoMagica =  Number(perfuracaoMagica) * Number(pontosAtributos);
+    let CalculoAtaqueMagico = (Number(ataqueMagico) * Number(pontosAtributos)).toFixed(2);
+    let CalculoCura = (Number(cura) * Number(pontosAtributos)).toFixed(2);
+    let CalculoResfriamento = (Number(resfriamento) * Number(pontosAtributos)).toFixed(2);
+    let CalculoPerfuracaoMagica =  (Number(perfuracaoMagica) * Number(pontosAtributos)).toFixed(2);
     
-  
     resultado.innerHTML = `${CalculoAtaqueMagico} Ataque mágico<br> 
                            ${CalculoCura} Cura<br>
                            ${CalculoResfriamento}% Resfriamento<br>
@@ -89,12 +84,11 @@ function atributosConstituicao(){
     let resistenciaAtaqueCritico = 0.1;
     let resistenciaAtaqueDuplo = 0.1;
 
-    let CalculoHpMaximo = Number(hpMaximo) * Number(pontosAtributos);
-    let CalculoRegeneracaoHp = Number(regeneracaoHp) * Number(pontosAtributos);
-    let CalculoResistenciaAtaqueCritico = Number(resistenciaAtaqueCritico) * Number(pontosAtributos);
-    let CalculoResistenciaAtaqueDuplo =  Number(resistenciaAtaqueDuplo) * Number(pontosAtributos);
+    let CalculoHpMaximo = (Number(hpMaximo) * Number(pontosAtributos)).toFixed(2);
+    let CalculoRegeneracaoHp = (Number(regeneracaoHp) * Number(pontosAtributos)).toFixed(2);
+    let CalculoResistenciaAtaqueCritico = (Number(resistenciaAtaqueCritico) * Number(pontosAtributos)).toFixed(2);
+    let CalculoResistenciaAtaqueDuplo =  (Number(resistenciaAtaqueDuplo) * Number(pontosAtributos)).toFixed(2);
     
-  
     resultado.innerHTML = `${CalculoHpMaximo} HP máximo<br> 
                            ${CalculoRegeneracaoHp} Regeneração de HP<br>
                            ${CalculoResistenciaAtaqueCritico} Resistência ataque crítico<br>
